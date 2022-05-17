fdeabdd75436b6144741b56091c7acd0184bd30b
Burak Tekcan Arslan <buraktekcan.arslan@medyasoft.com.tr>
2022-05-17 10:26:40 +0300

    Add project files.

---
+++ Merge Sort Projesi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Merge_Sort_Projesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
